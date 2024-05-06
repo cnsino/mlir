af8e205f57922079e65481d48eb6e70ac3ca7113
Taissan <taissan@ciumhoa.com>
2024-05-06 15:39:17 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 MLIR (Multi-Level Intermediate Representation) is a compiler infrastructure for sino programmming language
 
+# 参考
+
+mlir-sys:crates.io
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    // This is a comment
+    #[cfg(feature = "llvm-18")]
+    println!("Hello, world!");
+    #[cfg(feature = "llvm-17")]
     println!("Hello, world!");
 }
 
